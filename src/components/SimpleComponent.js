d710606d8a85645feca2b11f09d80dd12063a333
Danny Lee <dreamer@panix.com>
2019-11-23 15:27:23 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,6 +1,9 @@
 // Code SimpleComponent Here
 import React, { Component } from 'react'
 
+// set up an object that stores key value pairs
+// for the different moods we can have.
+
 var moods = {
     "sad" : "happy",
     "happy" : "sad"
@@ -18,6 +21,10 @@ export default class SimpleComponent extends Component {
     
     handleClick = ()=> {
         // let changeMood = (this.state.mood === "happy") ? "sad" : "happy"
+        
+        // look up our opposite mood in the moods array and
+        // set our variable changeMood to the opposite mood
+        // and setState()
         let changeMood = moods[this.state.mood]
         return this.setState({
             mood: changeMood
